71b07aebf261b15586d17cc6ff54ef6881e409ff
Jason Schleifer <jason.schleifer@gmail.com>
2025-01-17 15:38:36 -0800

    Update structure.py

--- structure.py
+++ structure.py
@@ -5,10 +5,24 @@ from dotenv import load_dotenv
 
 from griptape.structures import Agent
 from griptape.events import EventListener, EventBus
+from griptape.drivers import GriptapeCloudEventListenerDriver
 
 
 def is_running_in_managed_environment() -> bool:
     return "GT_CLOUD_STRUCTURE_RUN_ID" in os.environ
+
+def get_listener_api_key() -> str:
+    api_key = os.environ.get("GT_CLOUD_API_KEY", "")
+    if is_running_in_managed_environment() and not api_key:
+        print(
+            """
+              ****WARNING****: No value was found for the 'GT_CLOUD_API_KEY' environment variable.
+              This environment variable is required when running in Griptape Cloud for authorization.
+              You can generate a Griptape Cloud API Key by visiting https://cloud.griptape.ai/keys .
+              Specify it as an environment variable when creating a Managed Structure in Griptape Cloud.
+              """
+        )
+    return api_key
 
 if __name__ == "__main__":
   input = sys.argv[1]
