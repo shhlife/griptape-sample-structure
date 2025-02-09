a8c3d60be966156587a89e3e3dda8234c54fe91f
Jason Schleifer <jason.schleifer@gmail.com>
2025-02-09 17:03:28 +1300

    updated structure and readme.

--- structure.py
+++ structure.py
@@ -1,32 +1,31 @@
 from typing import List
 
 import typer
-from griptape.artifacts import ListArtifact, TextArtifact
 from griptape.structures import Agent
 from griptape.utils import GriptapeCloudStructure
 
 app = typer.Typer(add_completion=False)
+
+# If you want to run a Griptape Structure, set this to True
+# otherwise, if you're just running regular Python code, set it to False
+use_structure = True
 
 
 @app.command()
 def run(args: List[str] = typer.Argument(...)):
     """Run the agent with a prompt."""
     with GriptapeCloudStructure() as context:
-        # If you want to run a Griptape Structure, set this to True
-        # otherwise, if you're just running regular Python code, set it to False
-        use_structure = False
-
         if use_structure:
+            # Run the agent with the provided arguments
             agent = Agent()
             agent.run(args)
         else:
             # Run whatever code you want and then set the context.output with the response you want to send.
-            # The output can be a string, or an artifact. In this case, we'll use a ListArtifact.
-            output_artifact_msg = TextArtifact("Hello from a Griptape Cloud Structure.")
-            output_artifact_prompt = TextArtifact(args)
+            # The output can be a string, an Artifact, or a ListArtifact
+            output_msg = f"You sent: {args}"
 
-            task_output = ListArtifact([output_artifact_msg, output_artifact_prompt])
-            context.output = task_output
+            # Use context.output to publish the output message
+            context.output = output_msg
 
 
 if __name__ == "__main__":
